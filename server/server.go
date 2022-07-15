be8d3a34b77b7e9e2fbd4fe81f02b07ae15ea24b
kenny <kenny.luque.t@uni.pe>
2022-07-14 20:16:59 -0500

    fix : env var

--- server/server.go
+++ server/server.go
@@ -10,8 +10,9 @@ import (
 )
 
 type Config struct {
-	Port        string
-	DatabaseUrl string
+	PORT         string
+	JWT_SECRET   string
+	DATABASE_URL string
 }
 
 type Server interface {
@@ -29,12 +30,16 @@ func (b *Broker) Config() *Config {
 }
 
 func NewServer(ctx context.Context, config *Config) (*Broker, error) {
-	if config.Port == "" {
+	if config.PORT == "" {
 		return nil, errors.New("port is required")
 	}
 
-	if config.DatabaseUrl == "" {
+	if config.DATABASE_URL == "" {
 		return nil, errors.New("database is required")
+	}
+
+	if config.JWT_SECRET == "" {
+		return nil, errors.New("jwt is required")
 	}
 
 	broker := &Broker{
@@ -50,8 +55,8 @@ func (b *Broker) Start(binder func(s Server, r *chi.Mux)) {
 
 	binder(b, b.router)
 
-	log.Println("Starting server on Port", b.Config().Port)
-	if err := http.ListenAndServe(b.config.Port, b.router); err != nil {
+	log.Println("Starting server on Port", b.Config().PORT)
+	if err := http.ListenAndServe(b.config.PORT, b.router); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
