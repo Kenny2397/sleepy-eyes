3924741abed73cb96eb04b1657bee312a64fe289
kenny <kenny.luque.t@uni.pe>
2022-07-26 09:43:21 -0500

    feat[handlers] : root route

--- handlers/home.go
+++ handlers/home.go
@@ -10,16 +10,21 @@ import (
 type HomeResponse struct {
 	Message string `json:"message"`
 	Status  bool   `json:"status"`
+	// Dg      dgo.Dgraph `json:"dg"`
 }
 
 func HomeHandler(s server.Server) http.HandlerFunc {
+	// dg := database.NewClient()
+
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		// status 200 ok!
 		w.WriteHeader(http.StatusOK)
+		// fmt.Println(dg)
 		json.NewEncoder(w).Encode(HomeResponse{
 			Message: "Welcome to my server 🚀",
 			Status:  true,
+			// Dg:      *dg,
 		})
 	}
 }
